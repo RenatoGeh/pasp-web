8c461ffa0410bf1b252ffc83731efd10d5b5fd44
Renato Lui Geh <renatogeh@gmail.com>
2022-07-12 01:13:04 -0300

    editor: add example selector

--- js/editor.js
+++ js/editor.js
@@ -1,17 +1,32 @@
 import {EditorView, basicSetup} from "codemirror"
-import {tag} from "@lezer/highlight"
 import {HighlightStyle} from "@codemirror/language"
 
 import {pasp} from "./lang-pasp/dist/index.js"
 
 const editorConfig = EditorView.theme({
-  "&": {height: "300px"},
   ".cm-scroller": {overflow: "auto"},
-  ".cm-editor": {border: 2px solid black},
-  ".cm-editor.cm-focused": {border: 2px solid black}
 })
 
 let editor = new EditorView({
   extensions: [editorConfig, basicSetup, pasp()],
-  parent: document.body
+  doc: "0.5::a.",
+  parent: document.getElementById("editor"),
 })
+
+var examplesInput = document.getElementById("examples");
+
+examplesInput.addEventListener("change", function(e) {
+  var file = examplesInput.value
+  var req = new XMLHttpRequest();
+
+  req.open("GET", "./examples/" + file.toLowerCase() + ".lp", false);
+  req.send();
+
+  if (req.status == 200) {
+    var data = req.responseText;
+    editor.dispatch({changes: [
+      {from: 0, to: editor.state.doc.length},
+      {from: 0, insert: data}
+    ]});
+  }
+});
